67b4969a85bc5e63fad05cdc1cc9d7ae829cd96a
Dang <shubham.dang@siemens.com>
2019-11-22 17:10:09 +0530

    changes

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-combine(30, 26);
-console.log("hello");
-function combine(n1, n2) {
+//  let resultString: string
+var combinedAges = combine(30, 25, "as-number");
+console.log(combinedAges);
+var combinedNames = combine('4', "4", "as-text");
+console.log(combinedNames);
+function combine(n1, n2, resultString) {
     var result;
-    if (typeof n1 == "number" && typeof n2 == "number") {
-        result = n1 + n2;
+    if (typeof n1 === 'number' && typeof n2 === "number" || resultString === "as-number") {
+        result = +n1 + +n2;
     }
     else
         result = n1.toString() + n2.toString();
-    console.log(result);
+    return result;
 }
+//let res= combine(30,26,"as-text");
+//console.log(res);
--- app.ts
+++ app.ts
@@ -1,21 +1,31 @@
-combine(30,26);
 
-console.log("hello");
 
-function combine(n1:number | string,n2:number|string,resultString)
+//  let resultString: string
+type Combine_stringAnd_number = number |string;
+const combinedAges=combine(30,25,"as-number");
+console.log(combinedAges);
+
+
+const combinedNames=combine('4',"4","as-text");
+console.log(combinedNames);
+
+function combine(n1:Combine_stringAnd_number, n2:Combine_stringAnd_number,resultString: "as-number" | "as-text")
 {
     let result;
 
-    if(typeof n1 =="number" && typeof n2=="number")
+    if(typeof n1 === 'number' && typeof n2=== "number"|| resultString==="as-number")
     {
-        result=n1+n2;
+        result= +n1 + +n2;
     }
    else
    result=n1.toString()+n2.toString();
   
-  console.log(result); 
+  return result;
 }
 
 
-
-
+//let res= combine(30,26,"as-text");
+//console.log(res);
+type User={name:string} | string;
+let u1:User={name:'MAX'};
+u1='MIC'; 
