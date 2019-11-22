a85fbdf844770f49266789f83fd2e132daaebe6c
Dang <shubham.dang@siemens.com>
2019-11-22 13:35:43 +0530

    Playing with nos,strings and bool

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("AAAAAA");
+function add(n1, n2, showResult, phrase) {
+    var res = n1 + n2;
+    if (showResult) {
+        console.log(phrase + res);
+    }
+    else
+        return n1 + n2;
+}
+var number1 = 5;
+var number2 = 7.66;
+var viewResult = true;
+var phrase = "The Result is:";
+add(number1, number2, viewResult, phrase);
--- app.ts
+++ app.ts
@@ -1,1 +1,20 @@
-console.log("AAAAAA");
+function add(n1:number,n2:number,showResult:boolean,phrase:string)
+{
+    const res=n1+n2;
+    if(showResult)
+    {
+        console.log(phrase + res);
+        
+    }
+    else
+    return n1+n2 ;
+   
+}
+
+
+const number1=5;
+const number2=7.66;
+const viewResult=true;
+const phrase= "The Result is:";
+
+add(number1,number2,viewResult,phrase);
